9874236e754c17071e479b5c77a1a55312a9f977
sali1502 <sali1502@sudent.miun.se>
2024-08-15 18:34:38 +0200

    Statiska elemet och responsivitet OK

---
+++ src/js/nodal.js
@@ -0,0 +1,27 @@
+/* Litet fönster som öppnas med formulär för att uppdatera i menyn */
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("JavaScript är laddad");
+    
+    const modal = document.getElementById('menuModal');
+    const closeButton = document.querySelector('.close-button');
+
+    document.querySelectorAll('.menu-link').forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const category = this.getAttribute('data-category');
+            document.querySelector('#addMenuForm').setAttribute('data-category', category);
+            modal.style.display = 'block';
+        });
+    });
+
+    closeButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
